16cce29a4d02f4b4d3c8e569518e2814b8303b16
teo523 <teodoro.dan@gmail.com>
2020-05-25 17:38:41 +0100

    added airbrushes

--- public/sketch.js
+++ public/sketch.js
@@ -5,16 +5,19 @@ var cursorX;
 var cursorY;
 var headX=0;
 var particles = [];
+var filters= [];
 var osc = [];
 var isOn = [];
 let icon1,icon2,icon3,icon4;
 var option = 1;
 var vector = [];
 var timeSteps = 500;
-var thick = 1;
+var thick = 7;
 var bHeight;
 var tWidth;
 var tHeight;
+var icon10;
+let extracanvas;
 
 function preload() {
     icon1 = loadImage('images/icon1.png');
@@ -25,18 +28,25 @@ function preload() {
     icon6 = loadImage('images/pen.png');
     icon7 = loadImage('images/pen.png');
     icon8 = loadImage('images/pen.png');
+    icon9 = loadImage('images/airbrush.png');
+    icon10 = loadImage('images/airbrush.png');
+    icon11 = loadImage('images/airbrush.png');
+    icon12 = loadImage('images/airbrush.png');
+
 
 }
 
 function setup() {
     var canv = createCanvas(windowWidth, windowHeight);
+
     background(255,255,255);
 
     bHeight=height/6;
     tWidth=width/10;
     tHeight=height/10;
 
-   
+    extracanvas = createGraphics(windowWidth-tWidth, windowHeight-bHeight);
+    extracanvas.clear();
 
 
     var data = {
@@ -54,49 +64,120 @@ function newDrawing(data) {
 }
 
 function mousePressed() {
+    //if we are in the drawing rectangle, then create an osc
     if(mouseY<=5*height/6 && mouseX<width-tWidth){
-    cursorX = mouseX;
-    cursorY = mouseY;
-    particles.push(new Particle(mouseX, mouseY, thick));
-    if (option == 1){
-        osc.push(new p5.Oscillator('sine'));
-        lineColor.push([255,0,0]);
+        cursorX = mouseX;
+        cursorY = mouseY;
+        //Create a new particle object and an oscillator epending on the selected brush
+        particles.push(new Particle(mouseX, mouseY, thick, option));
+        if (option < 7)
+             filters.push(new p5.LowPass());
+        
+        
+        else
+            filters.push(new p5.HighPass());
+        if (option == 1){
+            osc.push(new p5.Oscillator('sine'));
+            lineColor.push([255,0,0]);
+        }
+        else if (option == 2){
+            osc.push(new p5.Oscillator('sawtooth'));
+            lineColor.push([180,0,60]);
+        }
+        else if (option == 3){
+            osc.push(new p5.Oscillator('triangle'));
+            lineColor.push([60,0,180]);
+        }
+        else if (option == 4){
+            osc.push(new p5.Oscillator('square'));
+            lineColor.push([0,0,255]);
+        }
+        else if (option == 5){
+            
+            filters[filters.length-1].freq(100);
+            filters[filters.length-1].res(5);
+            osc.push(new p5.Noise());
+            osc[osc.length-1].disconnect();
+            osc[osc.length-1].connect(filters[filters.length-1]);
+            lineColor.push([255,0,0]);
+        }
+        else if (option == 6){
+                
+            filters[filters.length-1].freq(100);
+            filters[filters.length-1].res(20);
+            osc.push(new p5.Noise('pink'));
+            osc[osc.length-1].disconnect();
+            osc[osc.length-1].connect(filters[filters.length-1]);
+            lineColor.push([180,0,60]);
+        }
+        else if (option == 7){
+            
+            filters[filters.length-1].freq(100);
+            filters[filters.length-1].res(5);
+            osc.push(new p5.Noise('brown'));
+            osc[osc.length-1].disconnect();
+            osc[osc.length-1].connect(filters[filters.length-1]);
+            lineColor.push([60,0,180]);
+        }
+        else if (option == 8){
+            
+            filters[filters.length-1].freq(100);
+            filters[filters.length-1].res(30);
+            osc.push(new p5.Noise());
+            osc[osc.length-1].disconnect();
+            osc[osc.length-1].connect(filters[filters.length-1]);
+            lineColor.push([0,0,255]);
+        }    
+        //starts in off mode
+        isOn.push(0);
     }
-    else if (option == 2){
-        osc.push(new p5.Oscillator('sawtooth'));
-        lineColor.push([180,0,60]);
-    }
-    else if (option == 3){
-        osc.push(new p5.Oscillator('triangle'));
-        lineColor.push([60,0,180]);
-    }
-    else if (option == 4){
-        osc.push(new p5.Oscillator('square'));
-        lineColor.push([0,0,255]);
-    }
-    isOn.push(0);
-}
+    
+    //else if mouse click is in right column
     else if (mouseY<=5*height/6 && mouseX>=width-tWidth){
+        //pens
         if (mouseY<tHeight)
             thick = 10;
-        if (mouseY>=tHeight && mouseY<2*tHeight)
+        else if (mouseY>=tHeight && mouseY<2*tHeight)
             thick = 7;
-        if (mouseY>=2*tHeight && mouseY<3*tHeight)
+        else if (mouseY>=2*tHeight && mouseY<3*tHeight)
             thick = 4;
-        if (mouseY>3*tHeight && mouseY<4*tHeight)
+        else if (mouseY>=3*tHeight && mouseY<4*tHeight)
             thick = 2;
+        //airbrushes
+        else if (mouseY>=4*tHeight && mouseY<5*tHeight)
+            thick = 20;
+        else if (mouseY>=5*tHeight && mouseY<6*tHeight)
+            thick = 17;
+        else if (mouseY>=6*tHeight && mouseY<7*tHeight)
+            thick = 14;
+        else 
+            thick = 12;
 
+        //update from pen to airbrush or opposite
+        if (thick > 10 && option < 5){
+            option += 4;
+            
+        }
+        else if (thick <= 10 && option > 4)
+            option -= 4;
+       console.log(option);
     }
-
+    //lower bar button update
     else{
         if (mouseX<width/4)
             option = 1;
-        if (mouseX>width/4 && mouseX<width/2)
+        else if (mouseX>width/4 && mouseX<width/2)
             option = 2;
-        if (mouseX>width/2 && mouseX<3*width/4)
+        else if (mouseX>width/2 && mouseX<3*width/4)
             option = 3;
-        if (mouseX>3*width/4)
+        else if (mouseX>3*width/4)
             option = 4;
+
+        if (thick > 10)
+            option+=4;
+
+
+    
     }
 
 }
@@ -114,8 +195,13 @@ function mouseDragged() {
     if(particles.length>0)
         particles[particles.length-1].update();
     
+    if (option > 4)
+        airbrush();
+
+    
+
  
-    noStroke();
+
     //fill(lineColor[0], lineColor[1], lineColor[2]);
     //ellipse(cursorX, cursorY, lineThickness, lineThickness);
 }
@@ -130,13 +216,17 @@ function draw() {
     stroke(0,0,0);
     line(headX,0,headX,height);
     headX+=10;
-    if (headX>=width)
+    if (headX>=width-tWidth)
         headX=0;
     playOscillators();
 
     for (var i = 0; i < particles.length; i++) {
-    particles[i].show(lineColor[i]);
+    if (particles[i].type < 5)
+        particles[i].show(lineColor[i]);
+
   }
+  image(extracanvas,0,0);
+
 }
 
 
@@ -144,14 +234,22 @@ function draw() {
 // Daniel Shiffman
 // code for https://youtu.be/vqE8DMfOajk
 
-function Particle(x, y, z) {
-  this.x = x;
-  this.y = y;
-  this.z = z;
+function Particle(x, y, z,option) {
+  
+    this.x = x;
+    this.y = y;
+    
+    if (z<= 10)
+        this.z = z;
+    else
+        this.z = z - 10;
+    
+    this.type = option;
 
-  this.history = [];
 
-  this.update = function() {
+    this.history = [];
+
+    this.update = function() {
     this.x = cursorX;
     this.y = cursorY;
 
@@ -197,8 +295,14 @@ var Playing = 0;
     for (var j=0;j<particles[i].history.length;j++) {
         if (abs(particles[i].history[j].x-headX)<5){
             Playing = 1;
-            osc[i].freq(map(particles[i].history[j].y,5*height/6,0,80,1000));
+            if(particles[i].type<=4)
+                osc[i].freq(map(particles[i].history[j].y,5*height/6,0,30,600));
+            else if (particles[i].type<=6)
+                filters[i].freq(map(particles[i].history[j].y,5*height/6,0,10,6000));
+            else 
+                filters[i].freq(map(particles[i].history[j].y,5*height/6,0,10,6000));
             osc[i].amp(map(particles[i].history[j].z,1,10,0.01,0.2));
+            
            if (isOn[i] == 0){
             //console.log("osc started");
             
@@ -289,7 +393,63 @@ function menuButtons() {
     image(icon8, width-tWidth/2-icon8.width/2,7*tHeight/2-icon8.height/2);
     noStroke();
 
+    let vOffset = height - bHeight - 8*tHeight;
+    translate(0, vOffset);
+
+    fill(130);
+    if (thick == 20)
+        stroke(255);
+    rect(width-tWidth,4*tHeight,tWidth,tHeight);
+    icon9.resize(2*tHeight/3,2* tHeight/3);
+    image(icon9, width-tWidth/2-icon9.width/2,9*tHeight/2-icon9.height/2);
+    noStroke();
+
+    fill(160);
+    if (thick == 17)
+        stroke(255);
+    rect(width-tWidth,5*tHeight,tWidth,tHeight);
+    icon10.resize(2*tHeight/3,2* tHeight/3);
+    image(icon10, width-tWidth/2-icon10.width/2,11*tHeight/2-icon10.height/2);
+    noStroke();
+
+    fill(190);
+    if (thick == 14)
+        stroke(255);
+    rect(width-tWidth,6*tHeight,tWidth,tHeight);
+    icon11.resize(tHeight/3,tHeight/3);
+    image(icon11, width-tWidth/2-icon11.width/2,13*tHeight/2-icon11.height/2);
+    noStroke();
+
+        fill(230);
+    if (thick == 12)
+        stroke(255);
+    rect(width-tWidth,7*tHeight,tWidth,tHeight);
+    icon12.resize(tHeight/4,tHeight/4);
+    image(icon12, width-tWidth/2-icon12.width/2,15*tHeight/2-icon12.height/2);
+    noStroke();
+
+    translate(0, -vOffset);
 
 
 }
 
+function airbrush() {
+
+    let max = 3* (thick-11);
+    let sz = random(5);
+    let spread = random(max);
+    let r3 = random(cursorX - spread, cursorX + spread);
+    let r4 = random(cursorY - spread, cursorY + spread);
+    extracanvas.noStroke();
+
+    console.log(option)
+    if (option == 5)
+        extracanvas.fill(255,0,0);
+    if (option == 6)
+        extracanvas.fill(180,0,60);
+    if (option == 7)
+        extracanvas.fill(60,0,180);
+    if (option == 8)
+        extracanvas.fill(0,0,255);
+    extracanvas.ellipse(r3, r4, sz, sz);
+}
